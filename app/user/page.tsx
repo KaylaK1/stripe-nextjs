abd7b434e8a9bd0720b3a561e076f452bd5727d9
KaylaK1 <44605348+KaylaK1@users.noreply.github.com>
2024-10-08 17:45:04 -0400

    added userprofile

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,10 +1,11 @@
 import LoginForm from "./LoginForm";
+import UserProfile from "./UserProfile";
 
 export default function Login() {
     return (
         <div>
             <h1>Login</h1>
-            <LoginForm />
+            <UserProfile />
         </div>
     );
 }
